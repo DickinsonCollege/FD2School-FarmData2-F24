f57a1534ce289fa34dc75f2bc19005d142644753
Toddthegod1 <klingert@dickinson.edu>
2024-10-21 16:31:33 -0400

    Added farm data tests except it can't seem to find the language

--- farmdata2/farmdata2_modules/fd2_school/e2e/e2e.report.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/e2e/e2e.report.spec.js
@@ -9,4 +9,11 @@ describe("Test the generateReport button", () => {
         cy.get("[data-cy=generate-report-button]").click()
         cy.get("[data-cy=report-header]").should("be.visible")
     })
+
+    it("Check the farm data", () => {
+        cy.get("[data-cy=generate-report-button]").click()
+        cy.get("[data-cy=farm-name]").should("contain.text", "Sample Farm")
+        cy.get("[data-cy=farm-user]").should("contain.text", "manager1")
+        cy.get("[data-cy=language]").should("have.text", "")
+    })
 })
