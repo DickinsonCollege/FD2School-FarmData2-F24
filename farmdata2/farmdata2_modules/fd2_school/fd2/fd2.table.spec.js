51d67d5817ca84cff1ea04d8e78483a45f368ac9
Hill134Z <zhongyu@dickinson@edu>
2024-10-26 23:23:33 -0400

    Added functionality of checking the rows in the table for the test

--- farmdata2/farmdata2_modules/fd2_school/fd2/fd2.table.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/fd2/fd2.table.spec.js
@@ -2,8 +2,14 @@ describe("Test the CustomTableComponent", () => {
   beforeEach(() => {
     cy.login("manager1", "farmdata2")
     cy.visit("/farm/fd2-school/fd2")
+    cy.get("[data-cy=generate-report-btn]").click()
   })
-  it("", () => {
-    
+  it("Check if the table has the right rows", () => {
+    cy.get("[data-cy=h0]").should("have.text","Row")
+    cy.get("[data-cy=h1]").should("have.text","Date")
+    cy.get("[data-cy=h2]").should("have.text","Area")
+    cy.get("[data-cy=h3]").should("have.text","Crop")
+    cy.get("[data-cy=h4]").should("have.text","Yield")
+    cy.get("[data-cy=h5]").should("have.text","Units")
   })
 })
