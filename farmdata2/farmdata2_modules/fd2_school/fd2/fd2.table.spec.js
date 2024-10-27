b0c2f5d684114d8b5b644c197da2b8951a486662
Hill134Z <zhongyu@dickinson@edu>
2024-10-26 23:19:54 -0400

    Added new empty test for CustomTableComponent

---
+++ farmdata2/farmdata2_modules/fd2_school/fd2/fd2.table.spec.js
@@ -0,0 +1,9 @@
+describe("Test the CustomTableComponent", () => {
+  beforeEach(() => {
+    cy.login("manager1", "farmdata2")
+    cy.visit("/farm/fd2-school/fd2")
+  })
+  it("", () => {
+    
+  })
+})
