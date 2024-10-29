fe80dc102172e1702eb858e278efa3bcd8fecc51
Ifrad <ifradm@dickinson.edu>
2024-10-29 17:42:40 -0400

    Verified the date values

--- farmdata2/farmdata2_modules/fd2_school/cypress/e2e/e2e.defaults.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/cypress/e2e/e2e.defaults.spec.js
@@ -2,12 +2,21 @@ describe("Test the harvest report default values", () => {
     beforeEach(() => {
         cy.login("manager1", "farmdata2")
         cy.visit("/farm/fd2-school/e2e")
-        cy.get("[data-cy=page-header]")
-            .should("have.text","Harvest Report")
+        
+        
 
     })
       
     it("Check the page header", () => {
+        cy.get("[data-cy=page-header]")
+            .should("have.text","Harvest Report")
     
     })
-})
+    it("Verify default values for date inputs", () => {
+        cy.get("[data-cy=start-date]")
+        .should("have.value", "2020-05-05")
+    cy.get("[data-cy=end-date]")
+        .should("have.value", "2020-05-15")
+        // Empty test, ready to be filled in later with assertions for default values
+    });
+});
