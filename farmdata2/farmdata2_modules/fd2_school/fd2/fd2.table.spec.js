c16950536ac76beecc0d9c10b32fc19d42ee9e2a
annepham1512 <phamn@dickinson.edu>
2024-10-31 18:57:54 -0400

    Add initial test setup for Harvest Report table in fd2.table.spec.js

---
+++ farmdata2/farmdata2_modules/fd2_school/fd2/fd2.table.spec.js
@@ -0,0 +1,9 @@
+describe("Harvest Report Table Tests", () => {
+  beforeEach(() => {
+    cy.visit("/farm/fd2-school/fd2"); // Update with the actual path for your FD2 sub-tab
+  });
+
+  it("should load the Harvest Report page", () => {
+    // Empty test to confirm Cypress setup
+  });
+});
