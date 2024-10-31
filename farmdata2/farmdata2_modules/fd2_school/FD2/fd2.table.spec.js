4de29505fcfed5713c7e28c208712ffea0ca6ba1
Ifrad <ifradm@dickinson.edu>
2024-10-31 16:26:59 -0400

    ran the tests

--- farmdata2/farmdata2_modules/fd2_school/FD2/fd2.table.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/FD2/fd2.table.spec.js
@@ -6,8 +6,17 @@ describe("Test the harvest report default values", () => {
         
 
     })
-    it("Check the page header", () => {
-        
-    
-    })
+    it('should display the correct table headers', () => {
+        // Visit the page where the Harvest Report is rendered
+        cy.visit("/farm/fd2-school/fd2");
+
+        // Click the "Generate Report" button to render the table
+        cy.get('[data-cy=generate-report-button]').click();
+
+        // Verify the table headers
+        const expectedHeaders = ["Date", "Area", "Crop", "Yield", "Units"];
+        expectedHeaders.forEach((header, index) => {
+            cy.get(`[data-cy=h${index}]`).should("have.text", header);
+        });
+    });
 });
