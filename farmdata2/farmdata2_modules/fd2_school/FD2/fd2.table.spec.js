941d6cdce991ba710f2491e5e714183785e254d9
Ifrad <ifradm@dickinson.edu>
2024-10-31 16:52:36 -0400

    Checked the wrong spec file previously but now the tests actually run

--- farmdata2/farmdata2_modules/fd2_school/FD2/fd2.table.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/FD2/fd2.table.spec.js
@@ -19,4 +19,20 @@ describe("Test the harvest report default values", () => {
             cy.get(`[data-cy=h${index}]`).should("have.text", header);
         });
     });
+    it('should display the correct table headers and number of columns', () => {
+        // Visit the page where the Harvest Report is rendered
+        cy.visit("/farm/fd2-school/fd2");
+
+        // Click the "Generate Report" button to render the table
+        cy.get('[data-cy=generate-report-button]').click();
+
+        // Verify the table headers
+        const expectedHeaders = ["Date", "Area", "Crop", "Yield", "Units"];
+        expectedHeaders.forEach((header, index) => {
+            cy.get(`[data-cy=h${index}]`).should("have.text", header);
+        });
+
+        // Check the number of columns
+        
+    });
 });
