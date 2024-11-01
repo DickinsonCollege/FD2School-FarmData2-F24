457780bbc85b85b784e8b37a5ce6a3ab33077dff
kateyvk <kateyvk16@gmail.com>
2024-11-01 11:25:42 -0400

    Fixed the cypress tests for crop dropDownWithAll Componenet and starting to implement the custom table component

--- farmdata2/farmdata2_modules/fd2_school/fd2/fd2.defaults.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/fd2/fd2.defaults.spec.js
@@ -4,23 +4,20 @@ describe("Test the harvest report default values", () => {
         cy.visit("/farm/fd2-school/fd2")
     })
       
-   
-    it("Checking the correct default start and end dates",()=>{
-        cy.get("[data-cy=start-date]")
-            .should("have.value","2020-05-05")
-        cy.get("[data-cy=end-date]")
-            .should("have.value","2020-05-15")
-    })
-    it("Sample testing the crop dropdown selection",()=>{
-        cy.get("[data-cy=crop-select]").children().eq(0)
-            .should("have.text","")
-        cy.get("[data-cy=crop-select]").children().eq(1)
-            .should("have.text","All")
-        cy.get("[data-cy=crop-select]").children().eq(5)
-            .should("have.text","BEAN-FAVA")
-        cy.get("[data-cy=crop-select]").children().eq(111)
-            .should("have.text","ZUCCHINI")
-        cy.get("[data-cy=crop-select]").children()
-            .should("have.length",112)
+
+    it("Sample testing the crop dropdown with all selection",()=>{
+
+        cy.get("[data-cy=crop-dropdown] > [data-cy=dropdown-input] > [data-cy=option0]")
+            .should("contain.text","All")
+        
+        cy.get("[data-cy=crop-dropdown] > [data-cy=dropdown-input] > [data-cy=option1]")
+            .should("contain.text","ARUGULA")
+
+        cy.get("[data-cy=crop-dropdown] > [data-cy=dropdown-input] > [data-cy=option5]")
+            .should("contain.text","BEAN-FAVA")
+        cy.get("[data-cy=crop-dropdown] > [data-cy=dropdown-input] > [data-cy=option111]")
+            .should("contain.text","ZUCCHINI")
+        cy.get("[data-cy=crop-dropdown] > [data-cy=dropdown-input]").children().should("have.length",112); 
+            
     })
 })
