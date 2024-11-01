e131497f58ae5dcaee4cbdeda2b7aefcdcd4611b
hmhngx <nguyehun@dickinson.edu>
2024-10-31 22:50:50 -0400

    Updated fd2.report.spec.js via vsCodium

--- farmdata2/farmdata2_modules/fd2_school/fd2/fd2.report.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/fd2/fd2.report.spec.js
@@ -1,47 +1,24 @@
-describe('Harvest Report Details Section', () => {
+describe('Harvest Report Crop Dropdown', () => {
     beforeEach(() => {
         cy.visit('/farm/fd2-school/fd2');
+        cy.get('[data-cy=generate-report-button]').click();
     });
 
-    it('should display default details before generating the report', () => {
-        cy.get('[data-cy="page-header"]').should('contain', 'Harvest Report');
+    it('should contain the correct crop options in the dropdown', () => {
+        cy.get('[data-cy=crop-dropdown] > [data-cy=dropdown-input]')
 
-        cy.get('[data-cy="farm-name"]').should('contain', 'Sample Farm');
-        cy.get('[data-cy="user-name"]').should('contain', 'manager1');
-        cy.get('[data-cy="language"]').should('contain', 'English');
-    });
+            .within(() => {
+                cy.get('[data-cy=option0]').should('have.text', 'All');
 
-    it('should display updated details after generating the report', () => {
-        cy.get('#title').clear().type('My Sample Harvest Report');
-        cy.get('[data-cy="start-date"]').type('2020-05-05');
-        cy.get('[data-cy="end-date"]').type('2020-05-15');
-        cy.get('[data-cy="crop_dropdown"]').select('kale');
+                cy.get('[data-cy=option1]').should('have.text', 'BROCCOLI');
 
-        cy.get('[data-cy="generate-report-button"]').click();
+                cy.get('[data-cy=option4]').should('have.text', 'KALE');
 
-        cy.get('[data-cy="farm-name"]').should('not.contain', 'Sample Farm'); 
-        cy.get('[data-cy="user-name"]').should('not.contain', 'manager1'); 
-        cy.get('[data-cy="language"]').should('not.contain', 'English'); 
-    });
+                cy.get('[data-cy=dropdown-input]').children().last().should('have.text', 'ZUCCHINI');
+            });
 
-    it('should display table data when there are matching records', () => {
-        cy.get('[data-cy="start-date"]').type('2020-05-05');
-        cy.get('[data-cy="end-date"]').type('2020-05-15');
-        cy.get('[data-cy="crop_dropdown"]').select('kale');
-
-        cy.get('[data-cy="generate-report-button"]').click();
-
-        cy.get('table').should('exist');
-        cy.get('table tr').should('have.length.greaterThan', 1); 
-    });
-
-    it('should display "No Matching Records" when there are no matching records', () => {
-        cy.get('[data-cy="start-date"]').type('2021-01-01');
-        cy.get('[data-cy="end-date"]').type('2021-01-02');
-        cy.get('[data-cy="crop_dropdown"]').select('corn');
-
-        cy.get('[data-cy="generate-report-button"]').click();
-
-        cy.contains('No Matching Records').should('be.visible');
+        cy.get('[data-cy=crop-dropdown] > [data-cy=dropdown-input]')
+            .children()
+            .should('have.length', 6);
     });
 });
