1c77e02f3814306a6929d3f70ba1277e1214e905
hemanthkapa <kapa4902@gmail.com>
2024-11-07 12:54:14 -0500

    all tests passed

--- farmdata2/farmdata2_modules/fd2_school/FD2/fd2.default.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/FD2/fd2.default.spec.js
@@ -1,35 +1,56 @@
-describe("Test the harvest report generation", () => {
+describe("Test the harvest report default values", () => {
     beforeEach(() => {
         cy.login("manager1", "farmdata2")
-        cy.visit("/farm/fd2-school/e2e")
+        cy.visit("/farm/fd2-school/fd2")
+    })
+      
+    it("Check the page header", () => {
+        cy.get("[data-cy=page-header]")
+            .should("have.text", "Harvest Report")
     })
 
-    it("Verify report generation functionality", () => {
-        // Check that report header doesn't exist before generating
+    it("Verify default values for date inputs", () => {
+        cy.get("[data-cy=start-date]")
+            .should("have.value", "2020-05-05")
+        cy.get("[data-cy=end-date]")
+            .should("have.value", "2020-05-15")
+    })
+
+    it('should find the crop dropdown element', () => {
+        cy.get('[data-cy=crop-select]')
+            .should('exist')
+    })
+    
+    it("Check the crop dropdown", () => {
+        // Wait for dropdown to exist and be populated
+        cy.get('[data-cy=crop-select]')
+            .should('exist')
+            .find('[data-cy=dropdown-input]')
+            .should('exist')
+
+        // Check first option is 'All'
+        cy.get("[data-cy=crop-select] > [data-cy=dropdown-input] > [data-cy=option0]")
+            .should("have.text", "All")
+
+        // Check second option is 'ARUGULA'
+        cy.get("[data-cy=crop-select] > [data-cy=dropdown-input] > [data-cy=option1]")
+            .should("have.text", "ARUGULA")
+
+        // Check fifth option is 'BEAN-DRY'
+        cy.get("[data-cy=crop-select] > [data-cy=dropdown-input] > [data-cy=option4]")
+            .should("have.text", "BEAN-DRY")
+
+        // Verify total number of options
+        cy.get("[data-cy=crop-select] > [data-cy=dropdown-input]")
+            .children()
+            .should("have.length", 112)
+    })
+
+    it("Check farm details are initially hidden", () => {
         cy.get("[data-cy=report-header]")
             .should("not.exist")
-        // Get and click the Generate Report button
-        cy.get("[data-cy=generate-report-button]")
-            .should("exist")
-            .click()
-        cy.get("[data-cy=report-header]")
-            .should("be.visible")
-    })
-
-    it("Check the farm data", () => {
-        cy.get("[data-cy=generate-report-button]")
-            .click()
             
-        // Approach 1: Testing full text content
         cy.get("[data-cy=farm-name]")
-            .should("have.text", "Farm: Sample Farm")
-            
-        // Approach 2: Using contains for partial match
-        cy.get("[data-cy=farm-user]")
-            .should("contain.text", "manager1")
-            
-        // Approach 3: Using span with have.text for precise testing
-        cy.get("[data-cy=language-value]")
-            .should("have.text", "English")
+            .should("not.exist")
     })
 })
